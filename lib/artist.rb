8a1402ba7085e08de6d8dc90d0547c267249e3bb
Noah A Klem <noahaklem@gmail.com>
2021-06-25 19:32:21 -0600

    commit tree 49b50f258f90a5b291e7517cf049e7839379511f

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,34 @@
+class Artist
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def genres
+        songs.map do |song|
+            song.genre 
+        end
+    end
+
+
+end
+
 
